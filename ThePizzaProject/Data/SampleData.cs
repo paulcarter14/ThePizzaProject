7e0339bb162b3c90a0f226ac313e6b9535359501
Mariesvensson <112252345+Mariesvensson@users.noreply.github.com>
2023-05-03 15:12:13 +0200

    New update ingredient category

--- ThePizzaProject/Data/SampleData.cs
+++ ThePizzaProject/Data/SampleData.cs
@@ -1,4 +1,5 @@
-﻿using ThePizzaProject.Models;
+﻿using ThePizzaProject.Migrations;
+using ThePizzaProject.Models;
 
 namespace ThePizzaProject.Data
 {
@@ -32,179 +33,240 @@ namespace ThePizzaProject.Data
 
             if (database.Ingredients.Count() == 0)
 			{
-				database.Ingredients.Add(new Ingredient
-				{
-					IngredientName = "Sourdough",
-					Amount= 100
-				});
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Sourdough",
+                    Category = "Dough",
+                    Amount = 100
+                }); ;
                 database.Ingredients.Add(new Ingredient
                 {
                     IngredientName = "Gluten-free",
+                    Category = "Dough",
                     Amount = 100
                 });
                 database.Ingredients.Add(new Ingredient
                 {
                     IngredientName = "Wheat",
-                    Amount = 100
-                }); database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Broccoli",
+                    Category = "Dough",
                     Amount = 100
                 });
+
+
+
+
                 database.Ingredients.Add(new Ingredient
                 {
                     IngredientName = "Mozzarella",
+                    Category = "Cheese",
                     Amount = 100
-                });
+                }) ; 
                 database.Ingredients.Add(new Ingredient
                 {
                     IngredientName = "Cheddar",
+                    Category = "Cheese",
                     Amount = 100
                 });
                
                 database.Ingredients.Add(new Ingredient
                 {
                     IngredientName = "Brie",
+                    Category = "Cheese",
                     Amount = 100
                 });
                 database.Ingredients.Add(new Ingredient
                 {
                     IngredientName = "Gorgonzola cheese",
+                    Category = "Cheese",
                     Amount = 100
                 });
                 database.Ingredients.Add(new Ingredient
                 {
                     IngredientName = "Feta cheese",
+                    Category = "Cheese",
                     Amount = 100
                 });
                 database.Ingredients.Add(new Ingredient
                 {
+                    IngredientName = "Gorgonzola cheese",
+                    Category = "Cheese",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Ricotta cheese",
+                    Category = "Cheese",
+                    Amount = 100
+                });
+
+
+
+
+                database.Ingredients.Add(new Ingredient
+                {
                     IngredientName = "Chicken",
+                    Category = "Protein",
                     Amount = 100
                 });
                 database.Ingredients.Add(new Ingredient
                 {
                     IngredientName = "Meatballs",
+                    Category = "Protein",
                     Amount = 100
                 });
                 database.Ingredients.Add(new Ingredient
                 {
                     IngredientName = "Shrimp",
-                    Amount = 100
-                }); database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Gorgonzola cheese",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Ham",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Prosciutto",
+                    Category = "Protein",
                     Amount = 100
                 });
 
                 database.Ingredients.Add(new Ingredient
                 {
-                    IngredientName = "Sausage",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Bacon",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Black Olives",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Green peppers",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Pineapple",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Jalapeños",
-                    Amount = 100
-                });
-            
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Tomatoes",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Anchovies",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Spinach",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Garlic",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Pepperoni",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Mushrooms",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Onions",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Sun-dried tomatoes",
-                    Amount = 100
-                });
-               
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Ricotta cheese",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Basil",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Pesto",
-                    Amount = 100
-                });
-                database.Ingredients.Add(new Ingredient
-                {
-                    IngredientName = "Red peppers",
+                    IngredientName = "Ham",
+                    Category = "Protein",
                     Amount = 100
                 });
                 database.Ingredients.Add(new Ingredient
                 {
                     IngredientName = "Chicken nuggets",
+                    Category = "Protein",
                     Amount = 100
                 });
-        
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Prosciutto",
+                    Category = "Protein",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Anchovies",
+                    Category = "Protein",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Sausage",
+                    Category = "Protein",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Bacon",
+                    Category = "Protein",
+                    Amount = 100
+                });
+
+
+
+
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Black Olives",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Green peppers",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Pineapple",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Jalapeños",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Tomatoes",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Spinach",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Garlic",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Pepperoni",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Mushrooms",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Onions",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Sun-dried tomatoes",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Basil",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Red peppers",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Broccoli",
+                    Category = "Vegetables",
+                    Amount = 100
+                });
+
+
+
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Kebab sauce",
+                    Category = "Sauces",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Garlic sauce",
+                    Category = "Sauces",
+                    Amount = 100
+                });
+                database.Ingredients.Add(new Ingredient
+                {
+                    IngredientName = "Pesto",
+                    Category = "Sauces",
+                    Amount = 100
+                });
             }
 
                 database.SaveChanges();
