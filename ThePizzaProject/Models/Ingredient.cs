96b50aa7b11a8a72e36d9fe61c79402ae8e24443
Mariesvensson <112252345+Mariesvensson@users.noreply.github.com>
2023-05-08 16:11:36 +0200

    newUpdate filter

--- ThePizzaProject/Models/Ingredient.cs
+++ ThePizzaProject/Models/Ingredient.cs
@@ -6,17 +6,13 @@ namespace ThePizzaProject.Models
     {
         [Key]
         public int IngredientID { get; set; }
-        //public int Amount { get; set; }
+        
         public string IngredientName { get; set; }
         public string Category { get; set; }
 
-        //public PizzaIngredient pizzaIngredients { get; set; }
+       
 
 		public List<PizzaIngredient> PizzaIngredients { get; set; }
-
-
-
-
 
 
 	}
