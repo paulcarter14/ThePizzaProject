349335afb7b3d48d49c689f6a4d3a054cc2739a9
Mariesvensson <112252345+Mariesvensson@users.noreply.github.com>
2023-05-24 10:07:48 +0200

    New update

--- ThePizzaProject/Migrations/20230524073757_first.cs
+++ ThePizzaProject/Migrations/20230524073757_first.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ThePizzaProject.Migrations
 {
-    public partial class Initial : Migration
+    public partial class first : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
